d3653bc328ab254b49fded8ba4b730ebee4caa62
Katlyn <kw17611@email.vccs.edu>
2024-08-25 21:03:40 +0000

    Added user name input

--- main.cpp
+++ main.cpp
@@ -7,17 +7,23 @@ first, middle, and last. Store your initials in these variables and display them
 */
 
 #include <iostream>
+#include <string>
 
 int main()
 {
 char firstInitial;
 char middleInitial;
 char lastInitial;
-std::cout << "Please enter the initial of your first name.";
+std::string firstName;
+  
+std::cout << "Please enter the initial of your first name:";
 std::cin >> firstInitial;
-std::cout << "Please enter the initial of your middle name.";
+std::cout << "Please enter the initial of your middle name:";
 std::cin >> middleInitial;
-std::cout << "Please enter the initial of your last name.";
+std::cout << "Please enter the initial of your last name:";
 std::cin >> lastInitial;
 std::cout << "Your initials are: " << firstInitial << middleInitial << lastInitial;
+std::cout << "Please enter your first name:";
+std::cin >> firstName;
+std::cout << "Welcome "<<firstName;
 }
